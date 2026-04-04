bf7cc96e7213be862298d0e323e011d3d383cbaa
Seoyeon <peaseoyean@gmail.com>
2026-04-04 16:08:17 +0000

    edit: Input 이미지 추가

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import textile
 from textile.utils.image_utils import read_and_process_image
 
 loss_textile = textile.Textile()
-image = read_and_process_image("CNN_Images/whole_map.png")
+image = read_and_process_image("CNN_Images/controlled_eval/perfect_tileable/Honny_house.jpg")
 textile_value = loss_textile(image)
 
 print("타일 점수:", textile_value.item())
