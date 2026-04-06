ecd2b275d6f6dbed214413aff0e399a3ea4e4933
Seoyeon <peaseoyean@gmail.com>
2026-04-06 18:20:26 +0000

    Add controlled evaluation images via Git LFS

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import textile
 from textile.utils.image_utils import read_and_process_image
 
 loss_textile = textile.Textile()
-image = read_and_process_image("CNN_Images/controlled_eval/perfect_tileable/Honny_house.jpg")
+image = read_and_process_image("CNN_Images/controlled_eval/Struct_broken/struct_broken.png")
 textile_value = loss_textile(image)
 
 print("타일 점수:", textile_value.item())
